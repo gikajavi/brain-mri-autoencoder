df6b6ad6dbbfb4da864476855e57ad3b5bf8b629
JAVI <gikajavi@gmail.com>
2021-12-11 19:14:08 +0100

    fix baseline model arch. definition. It was doing upsamplig after Conv2DTranspose due to a missconception.

--- pipeline/baseline_model.py
+++ pipeline/baseline_model.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import keras
 from keras import layers
 
-
 def baseline_model():
     input_img = keras.Input(shape=(128, 128, 1))
     x = layers.Conv2D(48, (3, 3), activation='relu', padding='same')(input_img)
@@ -14,13 +13,9 @@ def baseline_model():
     # This layer contains the latent space:
     encoded = layers.MaxPooling2D((2, 2), name='middle_layer')(x)
 
-    x = layers.Conv2DTranspose(192, (3, 3), activation='relu', padding='same')(encoded)
-    x = layers.UpSampling2D((2, 2))(x)
-    x = layers.Conv2DTranspose(192, (3, 3), activation='relu', padding='same')(x)
-    x = layers.UpSampling2D((2, 2))(x)
-    x = layers.Conv2DTranspose(96, (3, 3), activation='relu', padding='same')(x)
-    x = layers.UpSampling2D((2, 2))(x)
-    x = layers.Conv2DTranspose(48, (3, 3), activation='relu', padding='same')(x)
+    x = layers.Conv2DTranspose(192, (3, 3), strides=2, activation='relu', padding='same')(encoded)
+    x = layers.Conv2DTranspose(96, (3, 3), strides=2, activation='relu', padding='same')(x)
+    x = layers.Conv2DTranspose(48, (3, 3), strides=2, activation='relu', padding='same')(x)
     decoded = layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
     autoencoder = keras.Model(input_img, decoded, name="baseline")
