538062a1613ccecc9aed05eb2910b219c4a750af
JAVI <gikajavi@gmail.com>
2021-12-17 23:31:49 +0100

    fix error and small refactor

--- pipeline/experiment.py
+++ pipeline/experiment.py
@@ -4,10 +4,11 @@ import keras
 import pickle
 from dataset import DataGenerator
 
+
 class Experiment:
     _history = None
 
-    def __init__(self, name='Experiment', model: keras.Model = None, da_enabled=True, train_gen=None, val_gen=None,
+    def __init__(self, name='Exp', model: keras.Model = None, da_enabled=True, train_gen=None, val_gen=None,
                  skull_stripped=True, path_to_dataset='', path_to_results='', epochs=25, es_patience=5,
                  batch_size=128, optimizer="Adam", loss="mse", metrics="mse"):
         self.name = name
@@ -17,7 +18,7 @@ class Experiment:
         self.val_gen = val_gen
         self.skull_stripped = skull_stripped
         self.path_to_dataset = path_to_dataset
-        self.path_to_results = path_to_results,
+        self.path_to_results = path_to_results
         self.epochs = epochs
         self.es_patience = es_patience
         self.batch_size = batch_size
@@ -41,14 +42,14 @@ class Experiment:
         self.val_gen = DataGenerator(base_dir=path_to_val_slices, batch_size=self.batch_size,
                                      Augment=self.da_enabled)
 
-
     def get_name(self):
         """
         The name of this experiment, which depends on the model's name and some of the experiment parameters
         :return:
         """
-        da_config: str = 'with_da' if self.da_enabled else 'NO_da'
-        return f'{self.name}_{self.model.name}_ep{self.epochs}_bs{self.batch_size}_{da_config}'
+        da_config: str = 'da-yes' if self.da_enabled else 'da-no'
+        skull_mode: str = 'skull-stripped' if self.skull_stripped else 'full-skull'
+        return f'{self.name}_model-{self.model.name}_{skull_mode}_ep-{self.epochs}_bs-{self.batch_size}_{da_config}_loss-{self.loss}'
 
     def start(self):
         """
@@ -69,7 +70,6 @@ class Experiment:
             print("General error executing the experiment: " + str(ex))
             print(traceback.format_exc())
 
-
     def fit_model(self):
         # TODO: params should be configurable. accuracy does not make sense for this tasks. We should
         # use SSIM or something more appropriate
@@ -81,11 +81,10 @@ class Experiment:
                                            restore_best_weights=True)
 
         self._history = self.model.fit(self.train_gen,
-                                  epochs=self.epochs,
-                                  batch_size=self.batch_size,
-                                  validation_data=self.val_gen,
-                                  callbacks=[tensorboard_callback, es])
-
+                                       epochs=self.epochs,
+                                       batch_size=self.batch_size,
+                                       validation_data=self.val_gen,
+                                       callbacks=[tensorboard_callback, es])
 
     def save_results(self):
         filename = self.path_to_results + self.get_name() + '.h5'
@@ -94,3 +93,4 @@ class Experiment:
         filename = self.path_to_results + self.get_name() + '.history'
         with open(filename, 'wb') as file_pi:
             pickle.dump(self._history.history, file_pi)
+
